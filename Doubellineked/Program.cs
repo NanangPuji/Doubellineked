c9024216e066659d666d68706c21589ccf7c8276
Nanang Puji <nanangpuji762@gmail.com>
2022-11-14 20:24:34 +0700

    add bool dellNode

--- Doubellineked/Program.cs
+++ Doubellineked/Program.cs
@@ -1,66 +1,126 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace Doubellineked
+namespace double_linked_list
 {
     class Node
     {
-        /*Node class represents the mode of doubly Linked List.
+        /* Node class represents the node of doubly linked list.
          * it consists of the information part and links to
-         * its succeding and preceeding
+         * its succeeding and preceeding
          * in terms of next and previous */
         public int noMhs;
         public string name;
-        //point to the succeeding node
+        //ppoint to the succeding node
         public Node next;
-        //point to the preceeding node
+        //point to the precceeding node
         public Node prev;
     }
+
     class DoubleLinkedList
     {
         Node START;
 
         //constructor
-    }
-
-    public DoublelinkedList()
-    {
-        START = null;
-    }
-
-    public void addNode()
-    {
-        int nim;
-        string nm;
-        Console.Write("\nEnter the roll number of the student: ")
-        nim = Console.ReadLine();
-        Node newNode = new Node();
-        newNode.noMhs = nim;
-        newNode.name = nm;
-
-        //check if the LIst empty
-        if (START == null) || nim <== START.noMHs)
-                if ((START != null) && (nim == START.noMhs)
+        public DoubleLinkedList()
         {
-            Console.WriteLine("\nDuplicate number  not allowed");
-            return;
+            START = null;
         }
-        newNode.next = START;
-        if (START != null)
-            START.prev = null;
-        return true;
-    }
-}
-internal class Program
-    {
-        static void Main(string[] args)
+        public void addNode()
         {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                START = newNode;
+                return;
+            }
+            /*if the node is to be inserted at between two node */
+            Node previous, current;
+            for (current = previous = START; current != null && nim >= current.noMhs; previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            /* On the execution of the above for loop, prev and
+             * * current will point to those nodes
+             * * between wich the new node is to be insarted */
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be insarted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
+        public bool search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null &&
+                rollNo != current.noMhs; previous = current,
+                current = current.next) { }
+            return (current != null);
+        }
+        public bool dellNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (search(rollNo, ref previous, ref current) == false)
+                return false;
+            //the begining of data
+            if (current.next == null)
+            {
+                previous.next = null;
+                return true;
+            }
+            //node between two nodes in the list
+            if (current == START)
+            {
+                START = START.next;
+                if (START != null)
+                    START.prev = null;
+                return true;
+            }
+            /*if the to be deleted is in between the list then the following lines of executed. */
+            previous.next = current.next;
+            current.next.prev = previous;
+            return true;
         }
     }
-}
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+
