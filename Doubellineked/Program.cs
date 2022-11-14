2a47817dbe992a10645efbe37f4fc6d9537dde22
Nanang Puji <nanangpuji762@gmail.com>
2022-11-14 21:24:06 +0700

    add void ascending

--- Doubellineked/Program.cs
+++ Doubellineked/Program.cs
@@ -121,6 +121,18 @@ namespace double_linked_list
             else
                 return false;
         }
+        public void ascending()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is Empty");
+            else
+            {
+                Console.WriteLine("\nRecord in the ascending order of" + "roll number are:\n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.noMhs + "" + currentNode.name + "\n");
+            }
+        }
     }
 
         internal class Program
