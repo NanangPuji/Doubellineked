e4e469b45690b0ff5c1c028b2a48030121a25e45
Nanang Puji <nanangpuji762@gmail.com>
2022-11-14 17:19:52 +0700

    Add project files.

---
+++ Doubellineked/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubellineked
+{
+    class Node
+    {
+        /*Node class represents the mode of doubly Linked List.
+         * it consists of the information part and links to
+         * its succeding and preceeding
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeeding node
+        public Node next;
+        //point to the preceeding node
+        public Node prev;
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+
+        //constructor
+    }
+
+    public DoublelinkedList()
+    {
+        START = null;
+    }
+
+    public void
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
